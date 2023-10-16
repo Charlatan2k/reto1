72af43c27f69730ba7ca4a9a1c53e0b328d1ec7b
Charlatan2k <octavianmihai.calin@gmail.com>
2023-10-16 13:40:12 +0200

    Agregado archivos de reto 2 y nueva función de cuadrado

---
+++ index.js
@@ -0,0 +1,23 @@
+function suma(op1, op2){
+  return op1 + op2
+}
+
+function subs(op1, op2){
+  return op1 - op2
+}
+
+function mult(op1, op2){
+  return op1 * op2
+}
+
+function div(op1, op2){
+  return op1 / op2
+}
+
+function rq(op1){
+  let result = Math.sqrt(op1);
+  return result;
+}
+
+
+module.exports={suma, subs, mult, div, rq}
---
+++ run.js
@@ -0,0 +1,6 @@
+const myLib = require("./index.js");
+
+console.log(myLib.suma(4,2));
+console.log(myLib.subs(4,2));
+console.log(myLib.mult(4,2));
+console.log(myLib.div(4,2));
